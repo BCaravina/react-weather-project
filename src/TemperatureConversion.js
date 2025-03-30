c62e96f5158fe0e05b6449c58b62226023a914f6
BCaravina <168189345+BCaravina@users.noreply.github.com>
2025-03-30 15:32:46 -0300

    Fixed bug temperature not updating

--- src/TemperatureConversion.js
+++ src/TemperatureConversion.js
@@ -3,25 +3,24 @@ import "./TemperatureConversion.css";
 
 export default function TemperatureConversion({ celsius }) {
   const [unit, setUnit] = useState("C");
-  const [temperature, setTemperature] = useState(celsius);
 
   function convertToFahrenheit(event) {
     event.preventDefault();
     setUnit("F");
-    setTemperature((celsius * 9) / 5 + 32);
   }
 
   function convertToCelsius(event) {
     event.preventDefault();
     setUnit("C");
-    setTemperature(celsius);
   }
+
+  const temperature = unit === "C" ? celsius : (celsius * 9) / 5 + 32;
 
   return (
     <span className="TemperatureConversion">
       <span className="temperature">{Math.round(temperature)}</span>
       <span className="temperature-unit">
-        °{unit} |
+        °{unit} |{" "}
         {unit === "C" ? (
           <a href="/" onClick={convertToFahrenheit} className="unit-link">
             °F
