1a5904b7d43de2221079a0cbe276e3c387e2770c
BCaravina <168189345+BCaravina@users.noreply.github.com>
2025-03-31 15:58:04 -0300

    Created basic layout structure for 1 forecast day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,23 @@
 import React, { useState } from "react";
+import "./Forecast.css";
 
-export default function Forecast() {
-  return <p>Forecast will be here</p>;
+export default function Forecast(props) {
+  return (
+    <div className="Forecast">
+      <div className="row">
+        <div className="col">
+          <div className="Forecast-weekday">Tue</div>
+          <img
+            src={props.icon}
+            alt={props.description}
+            className="weather-icon"
+          />
+          <div className="Forecast-temperatures">
+            <span className="Forecast-temperature-min">10°</span>
+            <span className="Forecast-temperature-max">19°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
